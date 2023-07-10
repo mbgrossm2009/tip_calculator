979065b27fac5a999208d0b3f3a7c2bcf1db3be4
mbgrossm2009 <grossmanmatt@yahoo.com>
2023-07-10 17:25:04 -0400

    added reset button

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -10,51 +10,74 @@ function TipCalculator() {
   const [percentageOfTip, setPercentageOfTip] = useState("");
   const [submitted, setSubmitted] = useState(false);
   const [inputValue, setInputValue] = useState("");
+  const [percentage, setPercentage] = useState("");
+  const [selectedOption, setSelectedOption] = useState("");
+  const [reset, setReset] = useState("");
+
+  const handleReset = () => {
+    setTotalAmount("$");
+    setSelectedOption("");
+    setSubmitted(false);
+    setReset(true);
+  };
+
+  const handleSelectChange = (e) => {
+    setSelectedOption(e.target.value);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setSubmitted(true);
   };
   return (
-    <div className="myForm col-lg-6">
-      <h1 className="title"> Basic Tip Calculator </h1>
-      <Form onSubmit={handleSubmit}>
-        <Form.Group className="hi">
-          <Form.Label>Total Bill Amount</Form.Label>
-          <Form.Control
-            defaultValue="$"
-            type="integer"
-            className="input_fields"
-            name="totalAmount"
-            onChange={(e) => setTotalAmount(e.target.value)}
-          />
-        </Form.Group>
+    <div className="Container">
+      <div className="myForm col-lg-6">
+        <h1 className="title"> Basic Tip Calculator </h1>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group className="hi">
+            <Form.Label>Total Bill Amount</Form.Label>
+            <Form.Control
+              defaultValue="$"
+              type="integer"
+              className="input_fields"
+              name="totalAmount"
+              value={totalAmount}
+              onChange={(e) => setTotalAmount(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>
+              Please select the percentage of tip you would like to leave
+            </Form.Label>
 
-        <Form.Group>
-          <Form.Label>
-            Please enter the percentage of tip you would like to leave
-          </Form.Label>
+            <Form.Select
+              aria-label="Default select example"
+              value={selectedOption}
+              onChange={handleSelectChange}
+            >
+              <option>Select Tip Amount</option>
+              <option value="15">15%</option>
+              <option value="18">18%</option>
+              <option value="20">20%</option>
+            </Form.Select>
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+          <Button variant="primary" onClick={handleReset}>
+            Reset
+          </Button>
+        </Form>
 
-          <Form.Control
-            type="integer"
-            className="input_fields"
-            name="percentageOfTip"
-            onChange={(e) => setPercentageOfTip(e.target.value)}
-          />
-        </Form.Group>
-
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
-
-      {submitted && (
-        <div>
-          <p className="col-lg-6">
-            Amount of tip needed: ${totalAmount * (percentageOfTip / 100)}
-          </p>
-        </div>
-      )}
+        {submitted && (
+          <div>
+            <p className="col-lg-6">
+              Amount of tip needed: $
+              {totalAmount.replace("$", "") * (parseInt(selectedOption) / 100)}
+            </p>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
